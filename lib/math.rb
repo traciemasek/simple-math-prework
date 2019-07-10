80276b3234c787c4b6d4a3e38785b707e8045c03
Tracie Masek <traciemasek@gmail.com>
2019-07-10 09:46:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,18 +3,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  num.sqrt
 end
 
 def order_of_operation(num1, num2, num3, num4)
